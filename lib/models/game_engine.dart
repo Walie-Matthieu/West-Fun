177acd4cf6c0ea2b7e28e46f2f978c16edbf1b20
Walie-Matthieu <whoumad@gmail.com>
2026-07-13 19:57:31 +0200

    Few changes

--- lib/models/game_engine.dart
+++ lib/models/game_engine.dart
@@ -4,23 +4,32 @@ import 'package:west_fun/data/question_bank.dart';
 import 'package:west_fun/models/game_models.dart';
 
 class GameEngine {
-  GameEngine({required this.players, required this.mode, required this.theme, this.maxDelta = 10})
-      : assert(players.length > 1, 'At least 2 players are required'),
-        totalTurns = max(players.length * 3, 1);
+  GameEngine({
+    required this.players,
+    required this.mode,
+    required this.theme,
+    this.maxDelta = 10,
+    Random? random,
+  })  : assert(players.length > 1, 'At least 2 players are required'),
+        totalTurns = max(players.length * 3, 1),
+        _random = random ?? Random() {
+    _currentQuestion = _drawNextQuestion();
+  }
 
   final List<Player> players;
   final GameMode mode;
   final PartyTheme theme;
   final int maxDelta;
   final int totalTurns;
+  final Random _random;
+  final List<String> _questionQueue = [];
+  String? _lastQuestion;
+  late String _currentQuestion;
 
   int currentTurn = 0;
   int activePlayerIndex = 0;
 
-  String get currentQuestion {
-    final questions = questionBank[theme]?[mode] ?? const ['No question available'];
-    return questions[currentTurn % questions.length];
-  }
+  String get currentQuestion => _currentQuestion;
 
   Player get activePlayer => players[activePlayerIndex];
 
@@ -41,6 +50,9 @@ class GameEngine {
     activePlayer.score += delta;
     currentTurn += 1;
     activePlayerIndex = (activePlayerIndex + 1) % players.length;
+    if (!isFinished) {
+      _drawNextQuestion();
+    }
     return delta;
   }
 
@@ -52,6 +64,9 @@ class GameEngine {
     players[selectedPlayerIndex].score += maxDelta;
     currentTurn += 1;
     activePlayerIndex = (activePlayerIndex + 1) % players.length;
+    if (!isFinished) {
+      _drawNextQuestion();
+    }
     return maxDelta;
   }
 
@@ -79,11 +94,47 @@ class GameEngine {
   void skipTurn() {
     currentTurn += 1;
     activePlayerIndex = (activePlayerIndex + 1) % players.length;
+    if (!isFinished) {
+      _drawNextQuestion();
+    }
   }
 
   List<Player> get ranking {
     final sorted = [...players];
     sorted.sort((a, b) => b.score.compareTo(a.score));
     return sorted;
+  }
+
+  String _drawNextQuestion() {
+    final questions = questionBank[theme]?[mode];
+    if (questions == null || questions.isEmpty) {
+      _currentQuestion = 'No question available';
+      _lastQuestion = _currentQuestion;
+      return _currentQuestion;
+    }
+
+    if (questions.length == 1) {
+      _currentQuestion = questions.first;
+      _lastQuestion = _currentQuestion;
+      return _currentQuestion;
+    }
+
+    if (_questionQueue.isEmpty) {
+      _questionQueue
+        ..clear()
+        ..addAll(questions);
+      _questionQueue.shuffle(_random);
+
+      if (_lastQuestion != null && _questionQueue.first == _lastQuestion) {
+        final swapIndex = 1 + _random.nextInt(_questionQueue.length - 1);
+        final temp = _questionQueue[0];
+        _questionQueue[0] = _questionQueue[swapIndex];
+        _questionQueue[swapIndex] = temp;
+      }
+    }
+
+    _currentQuestion = _questionQueue.removeAt(0);
+    _lastQuestion = _currentQuestion;
+    return _currentQuestion;
   }
 }
